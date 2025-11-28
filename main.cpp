966a70d43c16414293c3578e85ad18aa0f9841ed
EgorPlotnikovRus <egorplotnikov69@gmail.com>
2025-11-28 15:47:38 +0700

    1

--- main.cpp
+++ main.cpp
@@ -19,8 +19,8 @@ int main(int argc, char** argv)
 
     generate_system(N, A, b, x_true);
 
-    std::cout << A(0, 0);
 
+    
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
